f7215a8bf842f1dc2c408be9a31846051c5dc516
dje <elliodj@verizon.net>
2021-02-27 15:03:32 -0500

    Add classes for access to Intel MKL.

---
+++ src/Ceres.Base/Math/MKL/MKLMethodsNative.cs
@@ -0,0 +1,133 @@
+﻿#region License notice
+
+/*
+  This file is part of the Ceres project at https://github.com/dje-dev/ceres.
+  Copyright (C) 2020- by David Elliott and the Ceres Authors.
+
+  Ceres is free software under the terms of the GNU General Public License v3.0.
+  You should have received a copy of the GNU General Public License
+  along with Ceres. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#endregion
+
+#region Using directive
+
+using System;
+using System.Runtime.InteropServices;
+using System.Security;
+
+#endregion
+
+namespace Ceres.Base.Math.MKL
+{
+  /// <summary>
+  /// Static interopability function for Intel MKL.
+  /// </summary>
+  [SuppressUnmanagedCodeSecurity]
+  public static unsafe class MKLMethodsNative
+  {
+    /// <summary>
+    /// Matrix transpose type.
+    /// </summary>
+    public enum BlasTransType { NoTrans = 111, Trans = 112, Packed = 151/* ? */ }
+    public enum BlasOrderType { Row = 101, Column = 102 }
+    public enum BlasMatrixID { A = 161, B = 162, C = 163 }
+    public enum BlasOffset { Row = 171, Col = 172, Fixed = 173 }
+
+    const bool MKL_VERBOSE = false;
+
+
+    // TODO: generalize this.
+    private const string dllName = @"C:\mkl\works\mkl_rt.dll";
+
+
+    static MKLMethodsNative()
+    {
+      // NOTE! Perforamnce is dramatically degraded without these settings.
+      System.Environment.SetEnvironmentVariable("KMP_AFFINITY", "physical");
+      System.Environment.SetEnvironmentVariable("MKL_NUM_THREADS", "1");
+
+      // Use this to turn on VERBOSE  mode, was not able to get the mkl_verbose API call to work.
+      if (MKL_VERBOSE) System.Environment.SetEnvironmentVariable("MKL_VERBOSE", "1");
+    }
+
+    [DllImport(dllName, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true, SetLastError = false)]
+    public static extern int vdExp(int N, [In] double[] A, [Out] double[] Y);
+
+    [DllImport(dllName, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true, SetLastError = false)]
+    public static extern int vdAdd(int N, [In] double[] A, [In] double[] B, [Out] double[] Y);
+
+    [DllImport(dllName, EntryPoint = "vdAdd", CallingConvention = CallingConvention.Cdecl, ExactSpelling = true, SetLastError = false)]
+    public static extern int vdAddPtr(int N, float* A, float* B, float* C);
+
+    [DllImport(dllName, EntryPoint = "vsAdd", CallingConvention = CallingConvention.Cdecl, ExactSpelling = true, SetLastError = false)]
+    public static extern int vsAddPtr(int N, float* A, float* B, float* C);
+
+    [DllImport(dllName, EntryPoint = "mkl_verbose", ExactSpelling = true, SetLastError = false, CallingConvention = CallingConvention.Cdecl)] // ** NOT WORKING (?)
+    public static extern void mkl_verbose(ref int verbose);
+
+    // PACKED (COULD NOT GET THIS TO WORK)
+    [DllImport(dllName, EntryPoint = "cblas_sgemm_alloc", ExactSpelling = true, SetLastError = false, CallingConvention = CallingConvention.Cdecl)]
+    public static extern IntPtr cblas_sgemm_alloc(BlasMatrixID matrixID, int m, int n, int k);
+
+    [DllImport(dllName, EntryPoint = "cblas_sgemm_pack", ExactSpelling = true, SetLastError = false, CallingConvention = CallingConvention.Cdecl)]
+    public static extern void cblas_sgemm_pack(BlasOrderType order, BlasMatrixID matrixID, BlasTransType transSrc, int m, int n, int k, float alpha, float[,] src, int ld, IntPtr dest);
+
+    [DllImport(dllName, EntryPoint = "cblas_sgemm_pack", ExactSpelling = true, SetLastError = false, CallingConvention = CallingConvention.Cdecl)]
+    public static extern void cblas_sgemm_pack1(BlasMatrixID matrixID, BlasTransType transSrc, int m, int n, int k, float alpha, float[,] src, int ld, IntPtr dest);
+
+    [DllImport(dllName, EntryPoint = "cblas_sgemm_compute", ExactSpelling = true, SetLastError = false, CallingConvention = CallingConvention.Cdecl)]
+    public static extern void cblas_compute(BlasOrderType order, BlasTransType transA, BlasTransType transB, int m, int n, int k, float[,] a,
+                                            int lda, /*float[,] b*/ IntPtr bPacked, int ldb, float beta, float[,] C, int ldc);
+
+    [DllImport(dllName, EntryPoint = "cblas_sgemm_free", ExactSpelling = true, SetLastError = false, CallingConvention = CallingConvention.Cdecl)]
+    public static extern void cblas_sgemm_free(IntPtr dest);
+    // END PACKED
+
+
+    [DllImport(dllName, EntryPoint = "cblas_sgemm", ExactSpelling = true, SetLastError = false, CallingConvention = CallingConvention.Cdecl)]
+    public static extern void cblas_sgemm2D(BlasOrderType order, BlasTransType transA, BlasTransType transB, int m, int n, int k, float alpha, float[,] a, int lda, float[,] b, int ldb, float beta, [In, Out] float[,] c, int ldc);
+
+    [DllImport(dllName, EntryPoint = "cblas_sgemm", ExactSpelling = true, SetLastError = false, CallingConvention = CallingConvention.Cdecl)]
+    public static extern void cblas_sgemm1D2D(BlasOrderType order, BlasTransType transA, BlasTransType transB, int m, int n, int k, float alpha, float[] a, int lda, float[,] b, int ldb, float beta, [In, Out] float[] c, int ldc);
+
+    // this method not in mklmkl, and uses "old style" pointers
+    [DllImport(dllName, EntryPoint = "mkl_scoomm", ExactSpelling = true, SetLastError = false, CallingConvention = CallingConvention.Cdecl)]
+    public static extern void cblas_scoomm(ref char transT, ref int m, ref int n, ref int k, ref float alpha,
+                                       char[] matdescra,
+                                       float[] values, int[] rowInd, int[] colInd, ref int numNonzero,
+                                       float[,] b, ref int ldb, ref float beta, [In, Out] float[,] c, ref int ldc);
+
+    [DllImport(dllName, EntryPoint = "mkl_scoomv", ExactSpelling = true, SetLastError = false, CallingConvention = CallingConvention.Cdecl)]
+    public static extern void cblas_scoomv(ref char transT, ref int m, ref int k, ref float alpha,
+                                       char[] matdescra,
+                                       float[] values, int[] rowInd, int[] colInd, ref int numNonzero,
+                                       float[,] b, ref float beta, [In, Out] float[] c);
+
+
+    [DllImport(dllName, EntryPoint = "cblas_sgemm", ExactSpelling = true, SetLastError = false, CallingConvention = CallingConvention.Cdecl)]
+    public static extern void cblas_sgemm_raw(BlasOrderType order, BlasTransType transA, BlasTransType transB, int m, int n, int k, float alpha, IntPtr a, int lda, IntPtr b, int ldb, float beta, [In, Out] IntPtr c, int ldc);
+
+
+    [DllImport(dllName, EntryPoint = "cblas_sgemv", ExactSpelling = true, SetLastError = false, CallingConvention = CallingConvention.Cdecl)]
+    public static extern void cblas_sgemv2D(BlasOrderType order, BlasTransType transA, int m, int n, float alpha, float[,] a, int lda, float[] x, int incX, float beta, [In, Out] float[] y, int incY);
+
+    [DllImport(dllName, ExactSpelling = true, SetLastError = false, CallingConvention = CallingConvention.Cdecl)]
+    public static extern void cblas_sgemv(BlasOrderType order, BlasTransType transA, int m, int n, float alpha, float[] a, int lda, float[] x, int incX, float beta, [In, Out] float[] y, int incY);
+
+
+    [DllImport(dllName, EntryPoint = "SSYEVR", ExactSpelling = true, SetLastError = false, CallingConvention = CallingConvention.Cdecl)]
+    public static extern void SSYEVR(ref char jobz, ref char range, ref char uplo, ref int n, [In] float[,] a, ref int lda,
+                                 [In] float[] vl, [In] float[] vu, [In] int[] il, [In] int[] iu, ref float abstol, ref int m, [In, Out] float[] w, [In, Out] float[,] z, ref int ldz,
+                                 [In, Out] int[] isuppz, [In, Out] float[] work, ref int len, [In, Out] int[] iwork, ref int ilen, ref int info);
+
+
+    [DllImport(dllName, EntryPoint = "cblas_gemm_s16s16s32", ExactSpelling = true, SetLastError = false, CallingConvention = CallingConvention.Cdecl)]
+    public static extern void cblas_gemm_s16s16s32(BlasOrderType order, BlasTransType transA, BlasTransType transB, MKLMethodsNative.BlasOffset offset,
+                                                   int m, int n, int k,
+                                                   float alpha, short[,] a, int lda, short ao,
+                                                   short[,] b, int ldb, short bo,
+                                                   float beta, int[] c, int ldc, int[] co);
+  }
+}
---
+++ src/Ceres.Base/Math/MKL/MKLUtils.cs
@@ -0,0 +1,237 @@
+﻿#region License notice
+
+/*
+  This file is part of the Ceres project at https://github.com/dje-dev/ceres.
+  Copyright (C) 2020- by David Elliott and the Ceres Authors.
+
+  Ceres is free software under the terms of the GNU General Public License v3.0.
+  You should have received a copy of the GNU General Public License
+  along with Ceres. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#endregion
+
+#region Using directive
+
+using System;
+
+#endregion
+
+namespace Ceres.Base.Math.MKL
+{
+  /// <summary>
+  /// Static helper methods related to Intel MKL library.
+  /// </summary>
+  public static unsafe class MKLUtils
+  {
+    /// <summary>
+    /// Matrix multiplication (GEMM).
+    /// </summary>
+    /// <param name="left"></param>
+    /// <param name="right"></param>
+    /// <param name="ret"></param>
+    /// <param name="overrideNumLeftRows">if not -1, indicates the number of rows in the left array to be used (the remainder are ignored)</param>
+    /// <returns></returns>
+    public static float[,] FloatMultGEMM(float[,] left, float[,] right, float[,] ret, int overrideNumLeftRows = -1)
+    {
+      int leftRowDim = overrideNumLeftRows == -1 ? left.GetLength(0) : overrideNumLeftRows;
+      int leftColDim = left.GetLength(1);
+      int rightColDim = right.GetLength(1);
+
+      MKLMethodsNative.cblas_sgemm2D(MKLMethodsNative.BlasOrderType.Row,
+                                      MKLMethodsNative.BlasTransType.NoTrans, MKLMethodsNative.BlasTransType.NoTrans,
+                                      leftRowDim, rightColDim, leftColDim, 1, left, leftColDim,
+                                      right, rightColDim, 0, ret, rightColDim);
+      return ret;
+    }
+
+
+    static int COUNT = 0;
+
+
+    /// <summary>
+    /// Matrix vector multiplication.
+    /// </summary>
+    /// <param name="left"></param>
+    /// <param name="right"></param>
+    /// <param name="ret"></param>
+    /// <param name="overrideNumLeftRows">if not -1, indicates the number of rows in the left array to be used (the remainder are ignored)</param>
+    /// <returns></returns>
+    public static float[] FloatMultGEMM1D(float[] left, float[,] right, float[] ret, int overrideNumLeftRows = -1)
+    {
+      int leftRowDim = 1;
+      int leftColDim = left.GetLength(0);
+      int rightColDim = right.GetLength(1);
+
+      // Yet to do: try using the version which does not check arguments for speed
+      MKLMethodsNative.cblas_sgemm1D2D(MKLMethodsNative.BlasOrderType.Row,
+                                      MKLMethodsNative.BlasTransType.NoTrans, MKLMethodsNative.BlasTransType.NoTrans,
+                                      leftRowDim, rightColDim, leftColDim, 1, left, leftColDim,
+                                      right, rightColDim, 0, ret, rightColDim);
+      return ret;
+    }
+
+
+    public static void FloatMultGEMM(float* left, float* right, int leftRowDim, int leftColDim, int rightRowDim, int rightColDim, float* ret)
+    {
+      if (leftColDim != rightRowDim) throw new Exception("wrong dim");
+
+      MKLMethodsNative.cblas_sgemm_raw(MKLMethodsNative.BlasOrderType.Row, MKLMethodsNative.BlasTransType.NoTrans, MKLMethodsNative.BlasTransType.NoTrans,
+                                      leftRowDim, rightColDim, leftColDim, 1, (IntPtr)left, leftColDim,
+                                      (IntPtr)right, rightColDim, 0, (IntPtr)ret, rightColDim);
+    }
+
+    #region Utils
+
+
+
+    [ThreadStatic] static float[] bufferValues = null;
+    [ThreadStatic] static int[] bufferCols = null;
+    [ThreadStatic] static int[] bufferRows = null;
+
+
+    /// <summary>
+    /// Sparse Matrix/Matrix multiplication.
+    /// </summary>
+    /// <param name="left"></param>
+    /// <param name="right"></param>
+    /// <param name="ret"></param>
+    /// <param name="maxPossibleNonzero"></param>
+    public static void SparseMatrixMatrixMult(float[,] left, float[,] right, float[,] ret, int maxPossibleNonzero)
+    {
+      if (bufferValues == null || bufferValues.Length != maxPossibleNonzero)
+      {
+        bufferRows = new int[maxPossibleNonzero];
+        bufferCols = new int[maxPossibleNonzero];
+        bufferValues = new float[maxPossibleNonzero];
+      }
+
+      // Use local copies of ThreadStatic variable references for efficiency.
+      int[] bufferColsLocal = bufferCols;
+      int[] bufferRowsLocal = bufferRows;
+      float[] bufferValuesLocal = bufferValues;
+
+      int numRows = left.GetLength(0);
+      int numCols = left.GetLength(1);
+
+      // Get nonzero entries.
+      // Note that it is not actually necessary to zero out
+      // the bufferValues which may not be filled because the MKL
+      // routine accepts the numActualNonzero as an argument.
+      int numActualNonzero = 0;
+      for (int i = 0; i < numRows; i++)
+      {
+        for (int j = 0; j < numCols; j++)
+        {
+          if (left[i, j] != 0)
+          {
+            if (numActualNonzero >= maxPossibleNonzero)
+              throw new Exception(maxPossibleNonzero + " maxPossibleNonzero value was insufficient");
+
+            bufferRowsLocal[numActualNonzero] = i;
+            bufferColsLocal[numActualNonzero] = j;
+            bufferValuesLocal[numActualNonzero] = left[i, j];
+            numActualNonzero++;
+          }
+        }
+      }
+
+      // "For general matrix you just put 'G' as first element, then 2nd, and 3rd are ignored anyway, so put anything, 
+      // and 'F'(for 1 - based indexing) or 'C'(for zero - based) as the last one"
+      char[] flg = new char[] { 'g', 'x', 'x', 'c' };
+      char fft = 'n';
+
+      int m = left.GetLength(0);
+      int n = right.GetLength(1);
+      int k = left.GetLength(1);
+      float alpha = 1.0f;
+      float beta = 0.0f;
+      MKLMethodsNative.cblas_scoomm(ref fft,
+                              ref m, ref n, ref k,
+                              ref alpha,
+                              flg, bufferValues, bufferRows, bufferCols, ref numActualNonzero,
+                              right, ref n, ref beta, ret, ref n);
+    }
+
+
+    /// <summary>
+    /// Performs spectral (Eignevector) decomposition.
+    /// </summary>
+    /// <param name="m"></param>
+    /// <param name="eigenvalues"></param>
+    /// <param name="calcEigenvectors"></param>
+    /// <param name="maxEigenvalues"></param>
+    /// <returns></returns>
+    public static float[,] Eigendecompose(float[,] m, ref float[] eigenvalues, bool calcEigenvectors, int maxEigenvalues = 0)
+    {
+      int NUM_ROWS = m.GetLength(0);
+      int NUM_COLS = m.GetLength(1);
+
+      char type = calcEigenvectors ? 'V' : 'N';
+      char range = 'I';
+      char target = 'U';
+      int lda = NUM_ROWS;
+
+      // Define return values
+      eigenvalues = new float[lda];
+      float[,] eigenvectors = calcEigenvectors ? new float[lda, lda] : null;
+
+      int eigenCount = 0;
+      float abstol = 0.0f;
+      int scratchLen = -1;
+      float[] scratch = new float[lda];
+      int scratchLenI = -1;
+      int[] scratchI = new int[lda];
+      float[] scratch1 = new float[1];
+      float[] scratch2 = new float[1];
+      int[] scratch1i = new int[1];
+      int[] scratch2i = new int[1];
+      int[] support = new int[lda * 2];
+
+      scratch1i[0] = NUM_ROWS - maxEigenvalues + 1;
+      scratch2i[0] = NUM_ROWS;
+
+      float[,] dataTranspose = Copy(m, true);
+
+      // Scratch area
+      int errCodeScratch = 0;
+      MKLMethodsNative.SSYEVR(ref type, ref range, ref target, ref lda, dataTranspose, ref lda,
+                                 scratch1, scratch2, scratch1i, scratch2i,
+                                 ref abstol, ref eigenCount, eigenvalues, eigenvectors, ref lda, support,
+                                 scratch, ref scratchLen, scratchI, ref scratchLenI, ref errCodeScratch);
+      if (errCodeScratch != 0) throw new Exception("SSYEVR MKL Failure:  " + errCodeScratch);
+
+      scratchLen = (int)scratch[0];
+      scratch = new float[scratchLen];
+      scratchLenI = (int)scratchI[0];
+      scratchI = new int[scratchLenI];
+
+      int errCode = 0;
+      MKLMethodsNative.SSYEVR(ref type, ref range, ref target, ref lda, dataTranspose, ref lda,
+                                 scratch1, scratch2, scratch1i, scratch2i,
+                                 ref abstol, ref eigenCount, eigenvalues, eigenvectors, ref lda, support,
+                                 scratch, ref scratchLen, scratchI, ref scratchLenI, ref errCode);
+      if (errCode != 0) throw new Exception("SSYEVR2D  MKL Failure: " + errCode);
+
+      Array.Reverse(eigenvalues);
+      return eigenvectors;
+    }
+
+
+    public static float[,] Copy(float[,] input, bool transpose)
+    {
+      int rows = input.GetLength(0);
+      int cols = input.GetLength(1);
+      float[,] ret = transpose ? new float[cols, rows] : new float[rows, cols];
+      for (int i = 0; i < rows; i++)
+        for (int j = 0; j < cols; j++)
+          if (!transpose)
+            ret[i, j] = input[i, j];
+          else
+            ret[j, i] = input[i, j];
+
+      return ret;
+    }
+
+  }
+}
