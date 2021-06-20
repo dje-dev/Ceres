a1e22f4084b6fe4237b46893cdda38f5c6330b8d
dje <elliodj@verizon.net>
2021-06-20 11:38:27 -0400

    Cleanup OptimalBatchSizeCalculator

--- src/Ceres.MCTS/Search/OptimalBatchSizeCalculator.cs
+++ src/Ceres.MCTS/Search/OptimalBatchSizeCalculator.cs
@@ -36,32 +36,38 @@ namespace Ceres.MCTS.Search
     ///   - more CPU overhead because the opportunity for parallelism in selecting the batch members is reduced
     ///     because parallelism overhead is prohibitively high with small concurrent sub-batch selection
     /// </summary>
-    /// <param name="totalNumNodes"></param>
-    /// <param name="currentNumNodes"></param>
+    /// <param name="estimatedTotalSearchNodes"></param>
+    /// <param name="currentTreeSize"></param>
     /// <param name="overlapInUse"></param>
     /// <param name="dualSelectorsInUse"></param>
     /// <param name="maxBatchSize"></param>
     /// <param name="batchSizeMultiplier"></param>
     /// <returns></returns>
-    internal static int CalcOptimalBatchSize(int totalNumNodes, int currentNumNodes, 
+    internal static int CalcOptimalBatchSize(int estimatedTotalSearchNodes, int currentTreeSize, 
                         bool overlapInUse, bool dualSelectorsInUse, 
                         int maxBatchSize, float batchSizeMultiplier = 1.0f)
     {
       // Follow pure MCTS for extremely small searches.
-      if (totalNumNodes < 10) return 1;
+      if (estimatedTotalSearchNodes < 10) return 1;
 
       // If tree size is very small, hand code reasonable batch size
       // such that we don't have many excessively small batches.
-      if (currentNumNodes <= 4) return 1;
-      if (currentNumNodes <= 7) return 2;
-      if (currentNumNodes <= 13) return 3;
+      switch (currentTreeSize)
+      {
+        case <= 4:
+          return 1;
+        case <= 7:
+          return 2;
+        case <= 13:
+          return 3;
+      }
 
       // At larger tree sizes, we have two components,
       // one of which has a low exponent and becomes meaningful only at larger tree sizes.
       // Note that play quality (with small number of nodes per moves, e.g. 5000) 
       // is quite sensitive to changes in these parameters, with larger clearly worse.
-      float part1 = 0.2f * MathF.Pow(currentNumNodes, 0.65f);
-      float part2 = 2.0f * MathF.Pow(currentNumNodes, 0.45f);
+      float part1 = 0.2f * MathF.Pow(currentTreeSize, 0.65f);
+      float part2 = 2.0f * MathF.Pow(currentTreeSize, 0.45f);
 
       int value = (int)(part1 + part2);
 
@@ -76,15 +82,21 @@ namespace Ceres.MCTS.Search
       }
 
       // Never allow batch size to be more than 10% of current number of nodes (or 20% for smaller trees)
-      float MAX_FRACTION = currentNumNodes < 100 ? 0.2f : 0.1f; // was 0.10
-      value = (int)(batchSizeMultiplier * Math.Min(value, (int)(MAX_FRACTION * currentNumNodes)));
+      float MAX_FRACTION = currentTreeSize < 100 ? 0.2f : 0.1f; // was 0.10
+      value = (int)(batchSizeMultiplier * Math.Min(value, (int)(MAX_FRACTION * currentTreeSize)));
 
       if (value < 1) // Cannot allow zero!
+      {
         return 1;
+      }
       else if (value > maxBatchSize)
+      {
         return maxBatchSize;
+      }
       else
+      {
         return value;
+      }
     }
 
   }
