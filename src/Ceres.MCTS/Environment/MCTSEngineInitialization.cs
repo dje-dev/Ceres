1ac7e10efdd00e09d58fd8abf53cc35ea4255829
dje <elliodj@verizon.net>
2021-08-14 22:05:44 -0400

    Set low semaphore spin limit for .NET 6 (reduced CPU usage).

--- src/Ceres.MCTS/Environment/MCTSEngineInitialization.cs
+++ src/Ceres.MCTS/Environment/MCTSEngineInitialization.cs
@@ -33,11 +33,21 @@ namespace Ceres.MCTS.Environment
     {
       HardwareManager.VerifyHardwareSoftwareCompatability();
 
+      // On .NET 6 the spin count for sempahores is directly configurable.
+      // Because most of the Ceres multithreading is not extremely fine grained,
+      // the awaited event almost always happens later than the default spinning period.
+      // Therefore it is better to use a short spin and save the CPU cycles.
+      // This reduces reported CPU time by about 15% to 20% with no slowdown.
+      // For .NET 6.0: see WorkerThread.cs, default was 70: AppContextConfigHelper.GetInt32Config("System.Threading.ThreadPool.UnfairSemaphoreSpinLimit", 70, false)
+      // (see for how to set this option: https://www.strathweb.com/2019/12/runtime-host-configuration-options-and-appcontext-data-in-net-core/)
+      AppDomain.CurrentDomain.SetData("System.Threading.ThreadPool.UnfairSemaphoreSpinLimit", 5);
+
       int minNumThreads = Math.Min(64, System.Environment.ProcessorCount * 2);
       System.Threading.ThreadPool.SetMinThreads(minNumThreads, 32);
 
       // TODO: consider using SustainedLowLatency when running under timed time control
       GCSettings.LatencyMode = GCLatencyMode.Batch;
+
 
       HardwareManager.Initialize(numaNode);
 
