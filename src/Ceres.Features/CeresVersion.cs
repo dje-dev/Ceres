357861c2159bb06c10a449fcb0d6d5a07efe4d8e
dje <elliodj@verizon.net>
2022-01-13 10:11:21 -0500

    Move version to 0.95-RC8.

--- src/Ceres.Features/CeresVersion.cs
+++ src/Ceres.Features/CeresVersion.cs
@@ -20,6 +20,6 @@ namespace Ceres.Features
 {
   public static class CeresVersion
   {
-    public const string VersionString = "0.95-RC7";
+    public const string VersionString = "0.95-RC8";
   }
 }
