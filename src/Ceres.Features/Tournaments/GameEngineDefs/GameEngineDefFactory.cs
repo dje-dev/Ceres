fbf5ffaaaefcd53dcd2fbd7a12eca94d22af7c2c
dje <elliodj@verizon.net>
2022-07-30 20:15:25 -0400

    Create GameEngineFactor, improvements to GameEngineDefFactory

--- src/Ceres.Features/Tournaments/GameEngineDefs/GameEngineDefFactory.cs
+++ src/Ceres.Features/Tournaments/GameEngineDefs/GameEngineDefFactory.cs
@@ -31,23 +31,22 @@ namespace Ceres.Features.GameEngines
     /// Returns a GameEngineDef for the Ceres engine running in this process.
     /// </summary>
     /// <param name="id"></param>
-    /// <param name="networkSpecification"></param>
-    /// <param name="deviceSpecification"></param>
+    /// <param name="networkSpec"></param>
+    /// <param name="deviceSpec"></param>
     /// <param name="paramsSearch"></param>
     /// <param name="paramsSelect"></param>
     /// <param name="logFileName"></param>
-    /// <param name="secondaryNetworkSpecification"></param>
+    /// <param name="secondaryNetworkSpec"></param>
     /// <returns></returns>
-    public static GameEngineDef CeresInProcess(string id, string networkSpecification, string deviceSpecification,
-                                               ParamsSearch paramsSearch = null, ParamsSelect paramsSelect = null,
-                                               string logFileName = null, string secondaryNetworkSpecification = null)
+    public static GameEngineDefCeres CeresInProcess(string id, string networkSpec, string deviceSpec,
+                                                    ParamsSearch paramsSearch = null, ParamsSelect paramsSelect = null,
+                                                    string logFileName = null, string secondaryNetworkSpec = null)
     {
       paramsSearch ??= new();
       paramsSelect ??= new();
 
-      NNEvaluatorDef netDef = NNEvaluatorDefFactory.FromSpecification(networkSpecification, deviceSpecification);
-      NNEvaluatorDef netDefSecondary = secondaryNetworkSpecification == null ?
-                                                                        null : NNEvaluatorDefFactory.FromSpecification(secondaryNetworkSpecification, deviceSpecification);
+      NNEvaluatorDef netDef = NNEvaluatorDefFactory.FromSpecification(networkSpec, deviceSpec);
+      NNEvaluatorDef netDefSecondary = secondaryNetworkSpec == null ? null : NNEvaluatorDefFactory.FromSpecification(secondaryNetworkSpec, deviceSpec);
 
       return new GameEngineDefCeres(id, netDef, netDefSecondary, paramsSearch, paramsSelect, null, logFileName);
     }
@@ -57,13 +56,13 @@ namespace Ceres.Features.GameEngines
     /// Returns a GameDefEngine for Ceres engine running in an external EXE (via UCI protocol).
     /// </summary>
     /// <param name="id"></param>
-    /// <param name="networkSpecification"></param>
-    /// <param name="deviceSpecification"></param>
+    /// <param name="networkSpec"></param>
+    /// <param name="deviceSpec"></param>
     /// <param name="exePath">path to Ceres EXE, or the executing assembly if null</param>
     /// <returns></returns>
-    public static GameEngineDef CeresUCI(string id, string networkSpecification, string deviceSpecification, string exePath = null)
+    public static GameEngineDefCeresUCI CeresUCI(string id, string networkSpec, string deviceSpec, string exePath = null)
     {
-      NNEvaluatorDef netDef = NNEvaluatorDefFactory.FromSpecification(networkSpecification, deviceSpecification);
+      NNEvaluatorDef netDef = NNEvaluatorDefFactory.FromSpecification(networkSpec, deviceSpec);
       return new GameEngineDefCeresUCI(id, netDef, overrideEXE: exePath);
     }
 
@@ -78,7 +77,7 @@ namespace Ceres.Features.GameEngines
     /// <param name="tbPath"></param>
     /// <param name="extraUCICommands"></param>
     /// <returns></returns>
-    public static GameEngineDef UCI(string id, string exePath,
+    public static GameEngineDefUCI UCI(string id, string exePath,
                                     int numThreads = 4, int hashSizeMB = 1024,
                                     string tbPath = null, List<string> extraUCICommands = null)
     {
@@ -91,15 +90,15 @@ namespace Ceres.Features.GameEngines
     /// Returns a GameDefEngine for a LC0 (Leela Chess Zero) chess engine running in an extrenal EXE (via UCI protocol).
     /// </summary>
     /// <param name="id"></param>
-    /// <param name="networkSpecification"></param>
-    /// <param name="deviceSpecification"></param>
+    /// <param name="networkSpec"></param>
+    /// <param name="deviceSpec"></param>
     /// <param name="forceDisableSmartPruning"></param>
     /// <param name="extraCommandLineArgs"></param>
     /// <returns></returns>
-    public static GameEngineDef LC0(string id, string networkSpecification, string deviceSpecification,
-                                    bool forceDisableSmartPruning = false, string extraCommandLineArgs = null)
+    public static GameEngineDefLC0 LC0(string id, string networkSpec, string deviceSpec,
+                                       bool forceDisableSmartPruning = false, string extraCommandLineArgs = null)
     {
-      NNEvaluatorDef netDef = NNEvaluatorDefFactory.FromSpecification(networkSpecification, deviceSpecification);
+      NNEvaluatorDef netDef = NNEvaluatorDefFactory.FromSpecification(networkSpec, deviceSpec);
       return new GameEngineDefLC0(id, netDef, forceDisableSmartPruning, null, extraCommandLineArgs: extraCommandLineArgs);
     }
 
---
+++ src/Ceres.Features/Tournaments/GameEngineDefs/GameEngineFactory.cs
@@ -0,0 +1,103 @@
+﻿#region License notice
+
+/*
+  This file is part of the Ceres project at https://github.com/dje-dev/ceres.
+  Copyright (C) 2020- by David Elliott and the Ceres Authors.
+
+  Ceres is free software under the terms of the GNU General Public License v3.0.
+  You should have received a copy of the GNU General Public License
+  along with Ceres. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#endregion
+
+#region Using directives
+
+using Ceres.Chess.GameEngines;
+using Ceres.MCTS.Params;
+using System.Collections.Generic;
+
+#endregion
+
+namespace Ceres.Features.GameEngines
+{
+  /// <summary>
+  /// Set of static factor methods to facilitate creating various types of GameEngine objects.
+  /// </summary>
+  public static class GameEngineFactory
+  {
+    /// <summary>
+    /// Returns a GameEngine for the Ceres engine running in this process.
+    /// </summary>
+    /// <param name="id"></param>
+    /// <param name="networkSpec"></param>
+    /// <param name="deviceSpec"></param>
+    /// <param name="paramsSearch"></param>
+    /// <param name="paramsSelect"></param>
+    /// <param name="logFileName"></param>
+    /// <param name="secondaryNetworkSpec"></param>
+    /// <returns></returns>
+    public static GameEngineCeresInProcess CeresInProcess(string id, string networkSpec, string deviceSpec,
+                                                          ParamsSearch paramsSearch = null, ParamsSelect paramsSelect = null,
+                                                          string logFileName = null, string secondaryNetworkSpec = null)
+    {
+
+      GameEngineDef def = GameEngineDefFactory.CeresInProcess(id, networkSpec, deviceSpec, paramsSearch, paramsSelect, 
+                                                              logFileName, secondaryNetworkSpec); ;
+      return def.CreateEngine() as GameEngineCeresInProcess;
+    }
+
+
+    /// <summary>
+    /// Returns a GameEngine for Ceres engine running in an external EXE (via UCI protocol).
+    /// </summary>
+    /// <param name="id"></param>
+    /// <param name="networkSpec"></param>
+    /// <param name="deviceSpec"></param>
+    /// <param name="exePath">path to Ceres EXE, or the executing assembly if null</param>
+    /// <returns></returns>
+    public static GameEngineUCI CeresUCI(string id, string networkSpec, string deviceSpec, string exePath = null)
+    {
+      GameEngineDefCeresUCI def = GameEngineDefFactory.CeresUCI(id, networkSpec, deviceSpec, exePath);
+      return def.CreateEngine() as GameEngineUCI;
+    }
+
+
+    /// <summary>
+    /// Returns a GameEngine for a generic engine running in an external EXE (via UCI protocol).
+    /// </summary>
+    /// <param name="id"></param>
+    /// <param name="exePath"></param>
+    /// <param name="numThreads"></param>
+    /// <param name="hashSizeMB"></param>
+    /// <param name="tbPath"></param>
+    /// <param name="extraUCICommands"></param>
+    /// <returns></returns>
+    public static GameEngineUCI UCI(string id, string exePath,
+                                    int numThreads = 4, int hashSizeMB = 1024,
+                                    string tbPath = null, List<string> extraUCICommands = null)
+    {
+      GameEngineDefUCI def = GameEngineDefFactory.UCI(id, exePath, numThreads, hashSizeMB, tbPath, extraUCICommands);
+      return def.CreateEngine() as GameEngineUCI;
+    }
+
+
+    /// <summary>
+    /// Returns a GameEngine for a LC0 (Leela Chess Zero) chess engine running in an extrenal EXE (via UCI protocol).
+    /// </summary>
+    /// <param name="id"></param>
+    /// <param name="networkSpec"></param>
+    /// <param name="deviceSpec"></param>
+    /// <param name="forceDisableSmartPruning"></param>
+    /// <param name="extraCommandLineArgs"></param>
+    /// <returns></returns>
+    public static GameEngineLC0 LC0(string id, string networkSpec, string deviceSpec,
+                                    bool forceDisableSmartPruning = false, string extraCommandLineArgs = null)
+    {
+      GameEngineDefLC0 def = GameEngineDefFactory.LC0(id, networkSpec, deviceSpec, forceDisableSmartPruning, extraCommandLineArgs);
+      return def.CreateEngine() as GameEngineLC0;
+    }
+
+  }
+
+}
