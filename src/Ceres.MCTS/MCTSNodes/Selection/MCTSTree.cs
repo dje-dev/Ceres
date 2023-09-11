a973aaff3400024fee8f657037c7c005c3954383
dje <elliodj@verizon.net>
2023-09-11 18:07:22 -0400

    Minor remediation for prior changes.

--- src/Ceres.MCTS/MCTSNodes/Selection/MCTSTree.cs
+++ src/Ceres.MCTS/MCTSNodes/Selection/MCTSTree.cs
@@ -128,7 +128,7 @@ namespace Ceres.MCTS.LeafExpansion
       // and will already be in the tree for all positions)
       for (int i = priorPositions.Length - 2; i >= 0; i--)
       {
-        EncodedPositionBoard thisBoard = EncodedPositionBoard.GetBoard(in priorPositions[i], priorPositions[i].MiscInfo.SideToMove);
+        EncodedPositionBoard thisBoard = EncodedPositionBoard.FromPosition(in priorPositions[i], priorPositions[i].MiscInfo.SideToMove);
         EncodedPriorPositions.Add(thisBoard);
       }
     }
@@ -344,7 +344,7 @@ namespace Ceres.MCTS.LeafExpansion
       const bool FAST = true;
       if (!FAST)
       {
-        annotation.LC0BoardPosition = EncodedPositionBoard.GetBoard(in posHistory[^1], annotation.PosMG.SideToMove);
+        annotation.LC0BoardPosition = EncodedPositionBoard.FromPosition(in posHistory[^1], annotation.PosMG.SideToMove);
       }
       else
       {
