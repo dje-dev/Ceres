96a2903f108222ff2447ee1c6e89c19fef6e4b27
dje <elliodj@verizon.net>
2023-12-04 09:57:40 -0500

    Add FileLogger class.

---
+++ src/Ceres.Base/Misc/FileLogger.cs
@@ -0,0 +1,81 @@
+﻿#region License notice
+
+/*
+  This file is part of the Ceres project at https://github.com/dje-dev/ceres.
+  Copyright (C) 2020- by David Elliott and the Ceres Authors.
+
+  Ceres is free software under the terms of the GNU General Public License v3.0.
+  You should have received a copy of the GNU General Public License
+  along with Ceres. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#endregion
+
+#region Using directives
+
+using System;
+using System.IO;
+
+#endregion
+
+namespace Ceres.Train
+{
+  /// <summary>
+  /// Manages logging of output to a file and/or console.
+  /// </summary>
+  public class FileLogger
+  {
+    // The name of the file to which output is logged.
+    public readonly string LiveLogFileName;
+
+    /// <summary>
+    /// If true, output is also sent to the console.
+    /// </summary>
+    public readonly bool Verbose;
+
+    /// <summary>
+    /// Number of lines output to the log file.
+    /// </summary>
+    public int NumLinesOutput { private set; get; } = 0;
+
+
+    /// <summary>
+    /// Constructor.
+    /// </summary>
+    /// <param name="logdir"></param>
+    /// <param name="liveLogFileName"></param>
+    /// <param name="verbose"></param>
+    /// <param name="fileHeaderString"></param>
+    public FileLogger(string logDir, string liveLogFileName = null, bool verbose = false, string fileHeaderString = null)
+    {
+      LiveLogFileName = Path.Combine(logDir, liveLogFileName ?? Path.GetRandomFileName() + ".txt");
+      Verbose = verbose;
+
+      if (fileHeaderString != null)
+      {
+        AddLine(liveLogFileName + Environment.NewLine + Environment.NewLine, true);
+      }
+    }
+
+
+    /// <summary>
+    /// Adds a line to the log file and optionally to the console.
+    /// </summary>
+    /// <param name="line"></param>
+    public void AddLine(string line, bool isPrefix = false)
+    {
+      string outLine = DateTime.Now + " " + line;
+      File.AppendAllText(LiveLogFileName, outLine + Environment.NewLine);
+
+      if (!isPrefix)
+      {
+        NumLinesOutput++;
+      }
+
+      if (Verbose)
+      {
+        Console.WriteLine(line);
+      }
+    }
+  }
+}
