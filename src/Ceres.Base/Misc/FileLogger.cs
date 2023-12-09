32596d9e90f85659cc62c708bfc23030c93f7813
dje <elliodj@verizon.net>
2023-12-09 11:11:35 -0500

    Ignore INFO: when present in a Python stack trace.

--- src/Ceres.Base/Misc/FileLogger.cs
+++ src/Ceres.Base/Misc/FileLogger.cs
@@ -97,7 +97,8 @@ namespace Ceres.Train
     {
       // Lines of the form: date time INFO: key value
       string[] logLines = File.ReadAllLines(LiveLogFileName);
-      logLinesStartingWithINFO = logLines.Where(line => line.Contains("INFO:"))
+      logLinesStartingWithINFO = logLines.Where(line => line.Contains("INFO:") 
+                                            && !line.Contains("print")) // ignore lines that may come from stack trace dump from Python of code rather than actually emitted
         .ToDictionary(line => line.Split(" ")[4], line => line.Split(" ")[^1]);
     }
 
