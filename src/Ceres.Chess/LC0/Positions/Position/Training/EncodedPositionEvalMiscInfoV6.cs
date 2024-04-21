4e2f54a271349091ec71edc0ea4a1a124344539f
dje <elliodj@verizon.net>
2024-04-20 20:19:46 -0400

    TAR data quality test improvements.

--- src/Ceres.Chess/LC0/Positions/Position/Training/EncodedPositionEvalMiscInfoV6.cs
+++ src/Ceres.Chess/LC0/Positions/Position/Training/EncodedPositionEvalMiscInfoV6.cs
@@ -203,7 +203,7 @@ namespace Ceres.Chess.EncodedPositions
     /// NOTE: It is recommended to use PlayedMove property at the parent structure (EncodedTrainingPosition.PlayedMove)
     ///       to avoid having deal with the internal details of the encoding (e.g .mirroring status).
     /// </summary>
-    internal readonly EncodedMove PlayedMove => EncodedMove.FromNeuralNetIndex(PlayedIndex);
+    public readonly EncodedMove PlayedMove => EncodedMove.FromNeuralNetIndex(PlayedIndex);
 
 
     /// <summary>
@@ -212,7 +212,7 @@ namespace Ceres.Chess.EncodedPositions
     /// NOTE: It is recommended to use BestMove property at the parent structure (EncodedTrainingPosition.BestMove)
     ///       to avoid having to deal with the internal details of the encoding (e.g .mirroring status).
     /// </summary>
-    internal readonly EncodedMove BestMove => EncodedMove.FromNeuralNetIndex(BestIndex);
+    public readonly EncodedMove BestMove => EncodedMove.FromNeuralNetIndex(BestIndex);
 
 
     /// <summary>
