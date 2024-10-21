299c9c3ccdbb42b82c87feca8e2ac8b44e86d4ec
dje <elliodj@verizon.net>
2024-10-21 09:26:20 -0400

    Cleanup in SANParser.

--- src/Ceres.Chess/Textual/SANParser.cs
+++ src/Ceres.Chess/Textual/SANParser.cs
@@ -14,14 +14,9 @@
 #region Using directives
 
 using System;
-using System.Collections.Generic;
-using System.Diagnostics;
 
 using Ceres.Chess.MoveGen;
-using Ceres.Chess.MoveGen.Converters;
 
-using static Ceres.Chess.PieceType;
-using static Ceres.Chess.SideType;
 
 #endregion
 
@@ -41,7 +36,7 @@ namespace Ceres.Chess.Textual
     /// <param name="square"></param>
     /// <param name="pos"></param>
     /// <returns></returns>
-    static (bool, Square) PawnOnFileBeforeSquare(Square square, Position pos)
+    static (bool, Square) PawnOnFileBeforeSquare(Square square, in Position pos)
     {
       int increment = pos.MiscInfo.SideToMove == SideType.White ? -1 : 1;
 
@@ -53,7 +48,9 @@ namespace Ceres.Chess.Textual
         nextSquare = Square.FromFileAndRank(square.File, nextRank);
         Piece pieceOnSquare = pos.PieceOnSquare(nextSquare);
         if (pieceOnSquare.Type == PieceType.Pawn && pieceOnSquare.Side == pos.MiscInfo.SideToMove)
+        {
           return (true, nextSquare);
+        }
 
       } while (true);
 
@@ -71,7 +68,8 @@ namespace Ceres.Chess.Textual
         Square thisFromSquare = new Square((int)move.FromSquareIndex, Square.SquareIndexType.BottomToTopRightToLeft);
         Square thisTargetSquare = new Square((int)move.ToSquareIndex, Square.SquareIndexType.BottomToTopRightToLeft); // TO DO: make a converter
 
-        bool originRankOrFileOk = originRankOrFileOrSquare == default || thisFromSquare.ToString().Contains(originRankOrFileOrSquare);
+        bool originRankOrFileOk = originRankOrFileOrSquare == default 
+                               || thisFromSquare.ToString().Contains(originRankOrFileOrSquare);
         if (thisTargetSquare == targetSquare && pos.PieceOnSquare(thisFromSquare) == piece && originRankOrFileOk)
         {
           return (true, thisFromSquare);
@@ -82,12 +80,16 @@ namespace Ceres.Chess.Textual
     }
 
 
-    static bool ParsePieceThenSquare(char pieceChar, string targetSquareStr, Position pos, string originRankOrFileOrSquare, bool withCapture,
+    static bool ParsePieceThenSquare(char pieceChar, string targetSquareStr, 
+                                     in Position pos, string originRankOrFileOrSquare, bool withCapture,
                                      PieceType promotionType, out Move move)
     {
       move = default;
 
-      if (originRankOrFileOrSquare != null) originRankOrFileOrSquare = originRankOrFileOrSquare.ToUpperInvariant();
+      if (originRankOrFileOrSquare != null)
+      {
+        originRankOrFileOrSquare = originRankOrFileOrSquare.ToUpperInvariant();
+      }
 
       Piece sourcePiece;
       sourcePiece = new Piece(pos.MiscInfo.SideToMove, Piece.PieceTypeFromChar(pieceChar));
@@ -108,7 +110,7 @@ namespace Ceres.Chess.Textual
     }
 
 
-    static Move ParseNormalSANMove(string str, Position pos)
+    static Move ParseNormalSANMove(string str, in Position pos)
     {
       string orgStr = str;
 
@@ -130,42 +132,65 @@ namespace Ceres.Chess.Textual
         'N' => PieceType.Knight,
         _ => PieceType.None
       };
-      if (promoPiece != PieceType.None) str = str[0..^1];
+
+      if (promoPiece != PieceType.None)
+      {
+        str = str[0..^1];
+      }
 
       if (str.Length == 2) // e.g. e4
       {
-        var (found, square) = PawnOnFileBeforeSquare(str, pos);
-        if (!found) throw new ArgumentException($"No eligible pawn found in SAN {str}");
+        (bool found, Square square) = PawnOnFileBeforeSquare(str, in pos);
+        if (!found)
+        {
+          throw new ArgumentException($"No eligible pawn found in SAN {str}");
+        }
         return new Move(square, str, promoPiece);
       }
       else if (str.Length == 3) // e.g. Nf3
       {
-        if (ParsePieceThenSquare(str[0], str.Substring(1), pos, null, false, promoPiece, out Move retMove)) return retMove;
+        if (ParsePieceThenSquare(str[0], str.Substring(1), in pos, null, false, promoPiece, out Move retMove))
+        {
+          return retMove;
+        }
       }
       else if (str.Length == 4 && str[1] == 'x') // e.g. Qxf6 or exc3
       {
         if (char.IsUpper(str[0])) // Qxf3
         {
-          if (ParsePieceThenSquare(str[0], str.Substring(2), pos, null, true, promoPiece, out Move retMove))
+          if (ParsePieceThenSquare(str[0], str.Substring(2), in pos, null, true, promoPiece, out Move retMove))
+          {
             return retMove;
+          }
         }
         else // e.g. exc3
         {
-          if (ParsePieceThenSquare('P', str.Substring(2), pos, str.Substring(0, 1), true, promoPiece, out Move retMove))
+          if (ParsePieceThenSquare('P', str.Substring(2), in pos, str.Substring(0, 1), true, promoPiece, out Move retMove))
+          {
             return retMove;
+          }
         }
       }
       else if (str.Length == 4 && str[1] != 'x') // e.g. Nbc5
       {
-        if (ParsePieceThenSquare(str[0], str.Substring(2), pos, str.Substring(1, 1), true, promoPiece, out Move retMove)) return retMove;
+        if (ParsePieceThenSquare(str[0], str.Substring(2), in pos, str.Substring(1, 1), true, promoPiece, out Move retMove))
+        {
+          return retMove;
+        }
       }
       else if (str.Length == 5 && str[2] == 'x') // e.g. Ncxd5
       {
-        if (ParsePieceThenSquare(str[0], str.Substring(3), pos, str.Substring(1, 1), true, promoPiece, out Move retMove)) return retMove;
+        if (ParsePieceThenSquare(str[0], str.Substring(3), in pos, str.Substring(1, 1), true, promoPiece, out Move retMove))
+        {
+          return retMove;
+        }
       }
       else if (str.Length == 5 && str[2] != 'x') // e.g.Bg7f8
       {
-        if (ParsePieceThenSquare(str[0], str.Substring(3), pos, str.Substring(1, 2), false, promoPiece, out Move retMove)) return retMove;
+        if (ParsePieceThenSquare(str[0], str.Substring(3), in pos, str.Substring(1, 2), false, promoPiece, out Move retMove))
+        {
+          return retMove;
+        }
       }
 
       throw new Exception($"Invalid SAN move {orgStr} from position {pos.FEN}");
@@ -183,21 +208,27 @@ namespace Ceres.Chess.Textual
     {
       // strip off possible "++" or "+"
       int indexFirstPlus = sanString.IndexOf("+", StringComparison.Ordinal);
-      if (indexFirstPlus != -1) sanString = sanString.Substring(0, indexFirstPlus);
+      if (indexFirstPlus != -1)
+      {
+        sanString = sanString.Substring(0, indexFirstPlus);
+      }
 
       // strip off possible "#"
       int indexFirstHash = sanString.IndexOf("#", StringComparison.Ordinal);
-      if (indexFirstHash != -1) sanString = sanString.Substring(0, indexFirstHash);
+      if (indexFirstHash != -1)
+      {
+        sanString = sanString.Substring(0, indexFirstHash);
+      }
 
       switch (sanString)
       {
         case null: throw new ArgumentNullException();
-        case "O-O": return new PositionWithMove(pos, new Move(Move.MoveType.MoveCastleShort));
-        case "O-O-O": return new PositionWithMove(pos, new Move(Move.MoveType.MoveCastleLong));
-        case "1-0": return new PositionWithMove(pos, new Move(Move.MoveType.WhiteWins));
-        case "0-1": return new PositionWithMove(pos, new Move(Move.MoveType.BlackWins));
-        case "1/2-1/2": return new PositionWithMove(pos, new Move(Move.MoveType.Draw));
-        default: return new PositionWithMove(pos, ParseNormalSANMove(sanString, pos));
+        case "O-O": return new PositionWithMove(in pos, new Move(Move.MoveType.MoveCastleShort));
+        case "O-O-O": return new PositionWithMove(in pos, new Move(Move.MoveType.MoveCastleLong));
+        case "1-0": return new PositionWithMove(in pos, new Move(Move.MoveType.WhiteWins));
+        case "0-1": return new PositionWithMove(in pos, new Move(Move.MoveType.BlackWins));
+        case "1/2-1/2": return new PositionWithMove(in pos, new Move(Move.MoveType.Draw));
+        default: return new PositionWithMove(in pos, ParseNormalSANMove(sanString, in pos));
       }
     }
 
