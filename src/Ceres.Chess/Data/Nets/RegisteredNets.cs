216f042cde8c9209243abaf1247754d7d83c69f1
dje <elliodj@verizon.net>
2025-06-30 08:20:15 -0400

    Add more RegisteredNets

--- src/Ceres.Chess/Data/Nets/RegisteredNets.cs
+++ src/Ceres.Chess/Data/Nets/RegisteredNets.cs
@@ -72,8 +72,13 @@ namespace Ceres.Chess.Data.Nets
         {"BT4_FP16_TRT", ONNXNet16LC0("BT4-1024x15x32h-swa-6147500.pb.gz_fp16", true)},
 
         // for BT5, TRT version fails in engine building!
-        {"BT5_FP16", ONNXNet16LC0("BT5-1024x15x32h-rpe-3700000-fixed-fp16.onnx", false)},
-
+        {"BT5_BIG_FP16", ONNXNet16LC0("BT5-1024x15x32h-rpe-3700000-fixed-fp16.onnx", true)},
+        {"BT5_SMALL_FP16", ONNXNet16LC0("BT5-1024x15x32h-rpe-swa-3700000.onnx", true)},
+        
+        //{"BT5_FP16", ONNXNet16LC0("BT5-1024x15x32h-rpe-swa-3700000.pb.gz.fp16.onnx", false)},
+        
+        {"BT5_FP16_QUANT", ONNXNet16LC0("BT5-int8.onnx", false)},
+        
         // From: https://storage.lczero.org/files/networks-contrib/big-transformers/
         {"BT4-1740_FP16_TRT", ONNXNet16LC0("BT4-1740.pb.gz_fp16", true) },
         {"BT4_SPSA_FP16_TRT", ONNXNet32LC0("bt4-newtune-3rdbranch-1130.pb.gz", true)},
