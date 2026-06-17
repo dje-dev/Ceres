a258d0313e6b19648bc656f4b70b53f0b0a7c2ff
dje <elliodj@verizon.net>
2026-06-17 07:55:30 -0400

    Add a comment in MCGSSelect.

--- src/Ceres.MCGS/Search/Phases/Select/MCGSSelect.cs
+++ src/Ceres.MCGS/Search/Phases/Select/MCGSSelect.cs
@@ -424,6 +424,7 @@ public class MCGSSelect
         }
         else
         {
+          // Tests confirm this reset very beneficial.
           parentNode.ResetQUsingSumWChildrenAndSelf(childStats.SumW, refreshSibling);
         }
       }
